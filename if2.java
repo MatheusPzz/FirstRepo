5f4fb9c8e68057940ef77d6037bba046029de89b
matheus perazzo <matheusperazzo2014@hotmail.com>
2022-08-04 14:06:22 +0100

    This is the first commit

---
+++ if2.java
@@ -0,0 +1,31 @@
+package pkgif;
+
+import java.util.Scanner;
+
+/**
+ * Program to demonstrate the use of a simple if statement
+ * @author Paul Kelly
+ *
+ */
+public class If {
+
+    public static void main(String[] args) {
+
+        double accountBalance;
+
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("What is your account balance? ");
+        accountBalance = kb.nextDouble();
+        
+        if (accountBalance < 0) {
+            System.out.println("Your account is in the red (overdrawn)");
+        }
+
+        if (accountBalance >= 0) {
+            System.out.println("Your account is in the black (credit)");
+        }
+
+    }
+
+}
